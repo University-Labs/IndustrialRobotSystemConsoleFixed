9a628956997587fd4726e1d921b5a489ef751910
Evster <8961240@gmail.com>
2021-02-15 14:32:33 +0700

    Added some tests

---
+++ test/TestControlSystem.cpp
@@ -0,0 +1,66 @@
+#include "gtest/gtest.h"
+#include "ControlSystem.h"
+#include "Robot.h"
+#include "ExecutiveUnitSpray.h"
+
+TEST(ControlSystem, addRobotSuccess)
+{
+    //arrange
+    ControlSystem sys;
+    IRobot* newRobot = new Robot("1", new ExecutiveUnitSpray(), new MovingUnit(10,10,10), new SensorUnit(255));
+
+    //act
+    bool insertRes = sys.addRobot(newRobot);
+
+    //assert
+    ASSERT_EQ(insertRes, true);
+    ASSERT_EQ(sys.getRobotMap().size(), 1);
+    ASSERT_EQ(sys.getRobot("1"), newRobot);
+}
+
+TEST(ControlSystem, addRobotFailed)
+{
+    //arrange
+    ControlSystem sys;
+    IRobot* newRobot = new Robot("1", new ExecutiveUnitSpray(), new MovingUnit(10,10,10), new SensorUnit(255));
+    IRobot* anotherRobot = new Robot("1" , new ExecutiveUnitSpray(), new MovingUnit(10,10,10), new SensorUnit(2));
+
+    //act
+    sys.addRobot(newRobot);
+    bool insertRes = sys.addRobot(anotherRobot);
+
+    //assert
+    ASSERT_EQ(insertRes, false);
+    ASSERT_EQ(sys.getRobotMap().size(), 1);
+    ASSERT_NE(sys.getRobot("1"), anotherRobot);
+}
+
+TEST(ControlSystem, eraseRobotSuccess)
+{
+    //arrange
+    ControlSystem sys;
+    IRobot* newRobot = new Robot("1", new ExecutiveUnitSpray(), new MovingUnit(10,10,10), new SensorUnit(255));
+
+    //act
+    sys.addRobot(newRobot);
+    bool eraseRes = sys.eraseRobot("1");
+
+    //assert
+    ASSERT_EQ(eraseRes, true);
+    ASSERT_EQ(sys.getRobotMap().size(), 0);
+}
+
+TEST(ControlSystem, eraseRobotFailed)
+{
+    //arrange
+    ControlSystem sys;
+    IRobot* newRobot = new Robot("1", new ExecutiveUnitSpray(), new MovingUnit(10,10,10), new SensorUnit(255));
+
+    //act
+    sys.addRobot(newRobot);
+    bool eraseRes = sys.eraseRobot("2");
+
+    //assert
+    ASSERT_EQ(eraseRes, false);
+    ASSERT_NE(sys.getRobotMap().size(), 0);
+}
--- test/TestRobot.cpp
+++ test/TestRobot.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "Robot.h"
 #include "ExecutiveUnitSpray.h"
+#include "ExecutiveUnitHand.h"
 
 TEST(Robot, turnOnSuccess)
 {
@@ -36,6 +37,27 @@ TEST(Robot, eraseCommandSuccess)
 {
     //arrange
     Robot *r = new Robot("123", new ExecutiveUnitSpray(), new MovingUnit(10, 10, 10), new SensorUnit(255));
+    int amount = 2;
+
+	//act
+    r->setPressureSensor(10);
+    r->moveMovingUnit(10, 10, 10, 100);
+    r->deactivateExecutiveUnit();
+
+    //удаление команды
+    bool successErase = r->undoCommand(1);
+
+	//assert
+	ASSERT_NE(r, nullptr);
+    ASSERT_EQ(successErase, true);
+	ASSERT_EQ(r->isCommandsEmpty(), false);
+    ASSERT_EQ(r->getAmountCommands(), amount);
+}
+
+TEST(Robot, eraseCommandFailed)
+{
+    //arrange
+    Robot *r = new Robot("123", new ExecutiveUnitSpray(), new MovingUnit(10, 10, 10), new SensorUnit(255));
     int amount = 3;
 
 	//act
@@ -43,13 +65,48 @@ TEST(Robot, eraseCommandSuccess)
     r->moveMovingUnit(10, 10, 10, 100);
     r->deactivateExecutiveUnit();
 
+    //удаление команды
+    bool failedErase = r->undoCommand(4);
+
 	//assert
 	ASSERT_NE(r, nullptr);
+    ASSERT_EQ(failedErase, false);
 	ASSERT_EQ(r->isCommandsEmpty(), false);
     ASSERT_EQ(r->getAmountCommands(), amount);
 }
 
-TEST(Robot, eraseCommandFailed)
+TEST(Robot, performCommandSuccess)
 {
+    //arrange
+    Robot *r = new Robot("123", new ExecutiveUnitSpray(), new MovingUnit(10, 10, 10), new SensorUnit(255));
+    string correctResponce = "1";
+    
+	//act
+    r->setPressureSensor(10);
+    r->turnOn();
+    string *responce = r->executeOneCommand();
 
+	//assert
+	ASSERT_NE(r, nullptr);
+    ASSERT_NE(responce, nullptr);
+    ASSERT_EQ(responce[0], correctResponce);
+	ASSERT_EQ(r->isCommandsEmpty(), true);
+}
+
+TEST(Robot, performCommandFailed)
+{
+    //arrange
+    Robot *r = new Robot("123", new ExecutiveUnitHand(), new MovingUnit(10, 10, 10), new SensorUnit(255));
+    string wrongResponce = "0";
+
+	//act
+    r->activateExecutiveUnit(5);
+    r->turnOn();
+    string *responce = r->executeOneCommand();
+
+	//assert
+	ASSERT_NE(r, nullptr);
+    ASSERT_NE(responce, nullptr);
+    ASSERT_EQ(responce[0], wrongResponce);
+	ASSERT_EQ(r->isCommandsEmpty(), false);
 }
